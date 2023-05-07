32ff3a6b2b35e4405f9313ecc012f5e730d30635
andrewtran274 <andrewtran274@gmail.com>
2023-05-07 22:23:22 +0700

    commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const img = document.querySelectorAll("img");
+
+// function autoSlider() {
+//   setTimeout(autoSlider, 2000);
+//   var pics;
+//   const img = document.querySelectorAll("img");
+//   for (pics = 0; pics < img.length; pics++) {
+//     img[pics].style.display = "none";
+//   }
+
+//   firstIndex++;
+//   if (firstIndex > img.length) {
+//     firstIndex = 1;
+//   }
+//   img[firstIndex - 1].style.display = "block";
+// }
+
+// autoSlider();
+const slider = () => {
+  let x = -1;
+};
+console.log(img);
